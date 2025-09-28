69bb53078e7e9f50798c97626731e7207b6e935d
Demond Balentine <demond.balentine@atlasschool.com>
2025-09-28 09:48:10 -0700

    Update App.js

--- client/App.js
+++ client/App.js
@@ -1,12 +1,14 @@
 // client/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
+import Send from './screens/Send';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/send" element={<Send />} />
         {/* Add more routes here */}
       </Routes>
     </Router>
