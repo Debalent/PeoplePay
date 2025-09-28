9dab56a842f3c1f9387160d32bb4e5f882903ea2
Demond Balentine <demond.balentine@atlasschool.com>
2025-09-28 10:12:19 -0700

    Update App.js

--- client/App.js
+++ client/App.js
@@ -6,6 +6,9 @@ import Receive from './screens/Receive';
 import Dashboard from './screens/Dashboard';
 import ParentalControls from './screens/ParentalControls';
 import Login from './screens/Login';
+import UserProfile from './screens/UserProfile';
+import ErrorPage from './screens/ErrorPage';
+import ThemeToggle from './components/ThemeToggle';
 import NavBar from './components/NavBar';
 
 export default function App() {
@@ -19,6 +22,9 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/controls" element={<ParentalControls />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/theme" element={<ThemeToggle />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
